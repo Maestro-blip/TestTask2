a849ba11ede5abdefb91d8a7c3f418a03824d5de
Maestro <volodimirkucak@icloud.com>
2025-12-24 14:09:11 +0200

    Add routers and JSON schema

--- server/src/router/auth.js
+++ server/src/router/auth.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const authController = require('../controller/auth.js')
+const validation = require('../middleware/validation.js')
 
-router.post('/register');
-router.post('/login');
+router.post('/register',validation.validDate,authController.register);
+router.post('/login',validation.validDate,authController.login);
 
 module.exports = router;
