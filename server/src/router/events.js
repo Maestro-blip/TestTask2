302ce1bf05089de0fa63efca145196ec377afeab
Maestro <volodimirkucak@icloud.com>
2025-12-24 16:41:15 +0200

    Update router

--- server/src/router/events.js
+++ server/src/router/events.js
@@ -2,16 +2,14 @@ const express = require('express');
 const router = express.Router();
 const eventController = require('../controller/event.js')
 const authMiddleware = require('../middleware/auth.js')
+
+
 router.post('/',authMiddleware.checkToken,eventController.create)
 router.get('/',eventController.read)
 router.get('/:id',eventController.readByID)
 
 router.delete('/:id',authMiddleware.checkToken,eventController.delete)
-/*
-router.post('/:id/register')
-router.get('/:id/participants')
-router.get('/registrations/me')
-*/
+
 
 
 
